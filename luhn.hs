4463e3738e02987c854a2bd8301f2a4e10870e58
Sebastian Dröge <sebastian@centricular.com>
2014-11-30 10:59:23 +0100

    Use zipWith instead of foldr . zip in luhn algorithm
    Also use swap . divMod instead of manually doing the same

--- luhn.hs
+++ luhn.hs
@@ -1,10 +1,11 @@
 import Data.List (unfoldr)
+import Data.Tuple (swap)
 
 luhn :: Integral a => a -> Bool
 luhn num = s `mod` 10 == 0
   where
     s = sum finalDigits
     finalDigits = map (\a -> if a > 9 then a - 9 else a) doubledDigits
-    doubledDigits = foldr (\(a, b) c -> (a*b) : c) [] $ zip reverseDigits (cycle [1,2])
-    reverseDigits = unfoldr (\a -> if a == 0 then Nothing else Just (a `mod` 10, a `div` 10)) num
+    doubledDigits = zipWith (*) reverseDigits (cycle [1,2])
+    reverseDigits = unfoldr (\a -> if a == 0 then Nothing else Just (swap . (`divMod` 10) $ a)) num
 
